fcbd379e062f5cf767b9cd1df2e38d8e61699b54
Cooper Mayne <coopermayne@gmail.com>
2017-10-03 16:52:44 -0700

    --amend

--- app/controllers/voice_messages_controller.rb
+++ app/controllers/voice_messages_controller.rb
@@ -1,6 +1,6 @@
 class VoiceMessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :authenticate_user!
+  skip_before_action :authenticate_user!
 
   before_action :set_voice_message, only: [:show, :edit, :update, :destroy]
 
