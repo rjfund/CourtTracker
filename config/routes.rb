d0a6a86bc77e1bc1e99be612259ca3f9a9dbafb5
Cooper Mayne <coopermayne@gmail.com>
2017-10-24 11:52:47 -0700

    custom devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   resources :voice_messages
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
   resources :cases
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
