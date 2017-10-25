bb7ec616ebae1116ed95bdde1777fa40bcde3e28
Cooper Mayne <coopermayne@gmail.com>
2017-10-25 15:42:25 -0700

    got the email confirmation working properly

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   resources :voice_messages
 
-  devise_for :users
-
   resources :cases
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   root to: 'cases#index'
+
+  devise_for :users, controllers: { confirmations: 'confirmations', registrations: 'registrations' }
+
 end
